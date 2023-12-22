12dbc3853d46f477dbf15cc848afe2bc9e8d342b
Sly <jarredgoddard@gmail.com>
2023-12-21 20:26:46 -0600

    updated vote logic

--- dist/vote/app/constants.php
+++ dist/vote/app/constants.php
@@ -5,21 +5,21 @@ define('site_title', 'Dawn');
 
 // the folder where this script is located. if in root, should just be / 
 // most likely will be /vote/
-define('web_root', '/vote/');
+define('web_root', 'vote/');
 
 // SQL Information. use the information provided in step 1.
-define('MYSQL_HOST', 'localhost'); # usually localhost
-define('MYSQL_DATABASE', 'primalps_vote');
-define('MYSQL_USERNAME', 'primalps_voteuser');
-define('MYSQL_PASSWORD', 'n(R5NEPV@%li');
+define('MYSQL_HOST', '68.178.222.132'); # usually localhost
+define('MYSQL_DATABASE', 'dawn_vote');
+define('MYSQL_USERNAME', 'dawn_reader');
+define('MYSQL_PASSWORD', '+y6$h81T5R[[');
 
 // enable or disable the user above. Should only
 // be used as a last resort.
 define("disable_root", false);
 
 // ROOT USER ACCESS. disabled by default. Enable by setting disable_root to false
-define('admin_username', 'primal_admin');
-define('admin_password', 'p_admin00000000001!~');
+define('admin_username', 'dawn_admin');
+define('admin_password', 'd_admin00000000001!~');
 
 
 // show/hide the admin button
@@ -31,7 +31,7 @@ define("dark_mode", 1);
 // CHANGE THIS
 define("api_key", "dfsdfsdfsdfds");
 
-DEFINE("players_online", file_get_contents($_SERVER["DOCUMENT_ROOT"]."/resources/players.txt"));
+DEFINE("players_online", file_get_contents( "../resources/players.txt"));
 # OPTIONS CREATED BY INSTALLER
 
 
